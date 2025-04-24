be3a2ce06a47e6650a2e86247fbd0ac16a3bf830
sc700 <135926047+sc700@users.noreply.github.com>
2025-04-24 16:18:11 -0600

    Update api_gateway_middleware.py

--- middleware/api_gateway_middleware.py
+++ middleware/api_gateway_middleware.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from starlette.middleware.base import BaseHTTPMiddleware
-
 from config import settings
 
 app = FastAPI()
@@ -10,12 +9,13 @@ class ApiGatewayAuthMiddleware(BaseHTTPMiddleware):
     def __init__(self, app: FastAPI):
         super().__init__(app)
 
-    def dispatch(self, request: Request, call_next):      
-        # all calls need api token, this is how we'll prevent random calls to our api
-        # not coming from aws api gateway
+    async def dispatch(self, request: Request, call_next):
+        # Check for the API token in the headers
         api_token_header = request.headers.get("x-api-token")
         if not api_token_header or api_token_header != settings.api_gateway_token:
-            # if not present or not matching, return 403 forbidden
-            return JSONResponse(status_code=403, content={"detail": "invalid api token"})         
+            # Return a 403 response if the token is missing or invalid
+            return JSONResponse(status_code=403, content={"detail": "Invalid API token"})
 
-        return call_next(request)
+        # If valid, proceed with the request
+        response = await call_next(request)
+        return response
